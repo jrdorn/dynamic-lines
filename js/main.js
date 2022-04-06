3f654ab2df32a88cf62858b3f93e4fd5a20448da
Josh Dorn <jr_dorn@yahoo.com>
2022-04-05 22:08:15 -0500

    Add floating drone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log(42);
+let container = document.querySelector("#container");
+let drone = document.querySelector("#drone");
+
+let droneCenter = {
+  x: drone.offsetWidth / 2,
+  y: drone.offsetHeight / 2,
+};
+
+container.addEventListener("mousemove", (e) => {
+  drone.style.transform = `translate3d(${e.offsetX - droneCenter.x}px, ${
+    e.offsetY - droneCenter.y
+  }px, 0)`;
+});
